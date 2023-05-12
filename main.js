0228d0a66e53d855d6f09ccb904bec2094ec0626
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-05-12 01:57:09 -0400

    Add favorite, unfavorite feature

--- main.js
+++ main.js
@@ -12,33 +12,81 @@ function addToFavorites(restaurantId) {
   }
   document.addEventListener('DOMContentLoaded', () => {
     const favoriteButtons = document.querySelectorAll('.favorite-btn');
-
+  
     favoriteButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            const card = event.target.closest('.card');
-            const restaurantId = card.id;
-            const restaurantName = card.querySelector('.box p').textContent;
-            const restaurantImage = card.querySelector('img').getAttribute('src');
-    
-            const restaurant = {
-                id: restaurantId,
-                name: restaurantName,
-                image: restaurantImage
-            };
-    
-            let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-            
-            // Check if the restaurant is already in the favorites list
-            const isAlreadyFavorite = favorites.some(fav => fav.id === restaurantId);
-    
-            // If it's not a duplicate, add it to the favorites
-            if (!isAlreadyFavorite) {
-                favorites.push(restaurant);
-                localStorage.setItem('favorites', JSON.stringify(favorites));
-                alert(`${restaurantName} has been added to favorites.`);
-            } else {
-                alert(`${restaurantName} is already in your favorites.`);
-            }
-        });
+      const card = button.closest('.card');
+      const restaurantId = card.id;
+  
+      // Check if the restaurant is already in the favorites list
+      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+      const isAlreadyFavorite = favorites.some(fav => fav.id === restaurantId);
+  
+      if (isAlreadyFavorite) {
+        button.innerHTML = '♥'; // Update the button icon to ♥ if already favorited
+      }
+  
+      button.addEventListener('click', (event) => {
+        const card = event.target.closest('.card');
+        const restaurantId = card.id;
+        const restaurantName = card.querySelector('.box p').textContent;
+  
+        let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  
+        // Check if the restaurant is already in the favorites list
+        const isAlreadyFavorite = favorites.some(fav => fav.id === restaurantId);
+  
+        if (!isAlreadyFavorite) {
+          // Add the restaurant to favorites
+          const restaurantImage = card.querySelector('img').getAttribute('src');
+          const restaurant = {
+            id: restaurantId,
+            name: restaurantName,
+            image: restaurantImage
+          };
+          favorites.push(restaurant);
+          button.innerHTML = '♥'; // Update the button icon to ♥
+          alert(`${restaurantName} has been added to favorites.`);
+        } else {
+          // Remove the restaurant from favorites
+          favorites = favorites.filter(fav => fav.id !== restaurantId);
+          button.innerHTML = '♡'; // Update the button icon to ♡
+          alert(`${restaurantName} has been removed from favorites.`);
+        }
+  
+        // Update the stored favorites in localStorage
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+      });
     });
-});
+  });
+  document.addEventListener('DOMContentLoaded', () => {
+    const favoriteButtons = document.querySelectorAll('.favorite-btn');
+  
+    favoriteButtons.forEach(button => {
+      button.addEventListener('click', event => {
+        event.stopPropagation(); // Stop event propagation to prevent card flip
+        toggleFavorite(button);
+        updateFavoriteSymbol(button);
+      });
+    });
+  });
+  
+  function toggleFavorite(button) {
+    if (button.innerHTML === '♡') {
+      button.innerHTML = '♥';
+    } else {
+      button.innerHTML = '♡';
+    }
+  }
+  
+  function updateFavoriteSymbol(button) {
+    const restaurantId = button.closest('.card').id;
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  
+    const isAlreadyFavorite = favorites.some(fav => fav.id === restaurantId);
+    if (isAlreadyFavorite) {
+      button.innerHTML = '♥';
+    } else {
+      button.innerHTML = '♡';
+    }
+  }
+  
